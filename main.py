ee6fec1a37e9b88feb6266601bfd01e0b73e3e5f
Clément DELATTRE <clement.delattre@epitech.eu>
2022-11-22 19:49:56 +0000

    Add commands: base_del to remove a base background task that check each stockpile and raise an alert is needed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 
 import utils
 from Base import Base
@@ -19,12 +20,8 @@ bases_list = dict()
 
 @bot.event
 async def on_ready():
+    update_stockpiles.start()
     print(f'Logged in as {bot.user} (ID:{bot.user.id})')
-
-
-@bot.command()
-async def test(ctx):
-    await ctx.send("test")
 
 
 @bot.command()
@@ -38,6 +35,17 @@ async def base_new(ctx, *, base_name: str = None):
     bases_list[base_name] = Base(base_name)
     await ctx.send(f"> La base {base_name} a été crée.")
 
+
+@bot.command()
+async def base_del(ctx, *, base_name: str = None):
+    if not base_name:
+        await ctx.send("> Le nom de la base n'a pas été précisé.\nCommande: `!base_new [nom de la base]`")
+        return
+    elif base_name not in bases_list.keys():
+        await ctx.send(f"> La base {base_name} n'existe pas.")
+        return
+    bases_list.pop(base_name, None)
+    await ctx.send(f"> La base {base_name} a été supprimé.")
 
 @bot.command()
 async def base_list(ctx):
@@ -91,6 +99,33 @@ async def base_stockpile(ctx, resource_type: str = None, stock: int = None, *, b
         return
     await ctx.send(
         f"> La base {base_name} a maintenant un stock de {resource_type} de {stock} unités.")
+
+
+@tasks.loop(seconds=10)
+async def update_stockpiles():
+    channel = bot.get_channel(1044270821029457964)
+
+    if len(bases_list.keys()) == 0:
+        return
+    for key in bases_list.keys():
+        stockpile = bases_list[key].get_maintenance_stockpile()
+        consumption_rate = bases_list[key].get_maintenance_consumption()
+        stockpile[0] -= consumption_rate[0]
+        stockpile[1] -= consumption_rate[1]
+
+        if stockpile[0] < 0:
+            stockpile[0] = 0
+        if stockpile[1] < 0:
+            stockpile[1] = 0
+
+        threshold = 5
+
+        if consumption_rate[0] > 0 and int(stockpile[0] / consumption_rate[0]) < threshold:
+            await channel.send(f"> Le stock de bsup restant ({stockpile[0]}) dans {key} est critique. Temps restant: {int(stockpile[0] / consumption_rate[0])}h.")
+        if consumption_rate[1] > 0 and int(stockpile[1] / consumption_rate[1]) < threshold:
+            await channel.send(f"> Le stock de gsup restant ({stockpile[1]}) dans {key} est critique. Temps restant: {int(stockpile[0] / consumption_rate[0])}h.")
+        print(key, "->", stockpile, consumption_rate)
+
 
 if __name__ == "__main__":
     bot.run(token)
