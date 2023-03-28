e29f9c35321722bc0dccf35611262713b1015b34
Clément DELATTRE <clement.delattre@epitech.eu>
2023-03-28 17:34:17 +0100

    Add utility package

---
+++ modules/utils/foxhole_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+FACTION_COLORS = {
+    'Warden': 0x245682,
+    'Colonial': 0x516C4B,
+    'Both': 0xffffff
+}
+
+class StockpileTypes(Enum):
+    STORAGE_DEPOT = '<:storagedepot:1077298889490694204>'
+    SEAPORT = '<:seaport:1077298856196313158>'
