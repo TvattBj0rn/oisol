2b3d098e74be66fa8e50c4d942b9cf8341660952
Clément DELATTRE <clement.delattre@epitech.eu>
2023-03-28 17:35:29 +0100

    Bot migration to commands

--- modules/stockpile_viewer/discord_stockpile_viewer.py
+++ modules/stockpile_viewer/discord_stockpile_viewer.py
@@ -1,20 +1,25 @@
 import discord
 from discord.ext import commands
 from modules.stockpile_viewer import StockpileViewerMenu, StockpileCreatorMenu, google_sheet_commands
+from modules.utils import foxhole_types
+from main import bot as oisol
 
-
-@commands.hybrid_command()
-async def view_stockpiles(ctx):
-    await ctx.defer()
+@oisol.tree.command(name='view_stockpiles')
+async def view_stockpiles(interaction: discord.Interaction):
+    await interaction.response.defer(ephemeral=True)
     try:
         stockpile_list = google_sheet_commands.get_all_stockpiles()
-
         sorted_stockpile_list = dict()
+
         for stockpile_name, stockpile_values in stockpile_list.items():
             if stockpile_values['localisation'] not in sorted_stockpile_list.keys():
                 sorted_stockpile_list[stockpile_values['localisation']] = list()
             sorted_stockpile_list[stockpile_values['localisation']].append({stockpile_name: stockpile_values})
-        embed = discord.Embed(title='Stockpiles', description='Current accessible stockpiles', color=0x245682)
+        embed = discord.Embed(
+            title='Stockpiles',
+            description='Current accessible stockpiles',
+            color=factions_settings.FACTION_COLORS['Warden']
+        )
 
         for localisation, stockpile_list in sorted_stockpile_list.items():
             stockpiles_here = str()
@@ -26,29 +31,32 @@ async def view_stockpiles(ctx):
                 inline=False
             )
 
-        await ctx.send(embed=embed)
+        await interaction.followup.send(embed=embed)
     except discord.ext.commands.HybridCommandError as e:
-        await ctx.send(e)
+        await interaction.followup.send(e)
 
 
-@commands.hybrid_command()
-async def view_stockpile(ctx, stockpile_name: str):
+@oisol.tree.command(name='view_stockpile')
+async def view_stockpile(interaction: discord.Interaction, stockpile_name: str):
+    if not stockpile_name:
+        await interaction.response.send_message('> Command is missing a parameter: `/view_stockpile stockpile_name`', ephemeral=True)
+        return
     menu = StockpileViewerMenu.StockpileViewerMenu(stockpile_name)
     embed = menu.set_home_page()
-    await ctx.send(embed=embed, view=menu, ephemeral=True)
+    await interaction.response.send_message(embed=embed, view=menu, ephemeral=True)
 
 
-@commands.hybrid_command()
-async def create_stockpile(ctx, code: str='0', *, name:str=''):
+@oisol.tree.command(name='create_stockpile')
+async def create_stockpile(interaction: discord.Interaction, code: str='0', *, name:str=''):
     if not name or not code:
-        await ctx.send('> Command is missing a parameter: `$create_stockpile code name` or `/create_stockpile code name`', ephemeral=True)
+        await interaction.response.send_message('> Command is missing a parameter: `/create_stockpile code name`', ephemeral=True)
         return
 
     view = StockpileCreatorMenu.StockpileCreatorMenu(code, name)
-    await ctx.send(view=view, ephemeral=True)
+    await interaction.response.send_message(view=view, ephemeral=True)
 
 
 async def setup(bot):
-    bot.add_command(view_stockpiles)
-    bot.add_command(view_stockpile)
-    bot.add_command(create_stockpile)
+    bot.tree.add_command(view_stockpiles)
+    bot.tree.add_command(view_stockpile)
+    bot.tree.add_command(create_stockpile)
--- modules/vehicles_stats/discord_wiki.py
+++ modules/vehicles_stats/discord_wiki.py
@@ -1,57 +1,109 @@
 import discord
-from modules.vehicles_stats import fstats_utils
-from discord.ext import commands
+from modules.vehicles_stats import wiki_utils
+from modules.utils import foxhole_types
+from main import bot as oisol
 
-
-@commands.hybrid_command()
-async def fstats(ctx, *, vehicle_name: str=''):
+@oisol.tree.command(name='wiki')
+async def wiki(interaction: discord.Interaction, *, vehicle_name: str=''):
     if not vehicle_name:
-        await ctx.send('> Command is missing a parameter: `$fstats vehicle_name` or `/fstats vehicle_name`', ephemeral=True)
+        await interaction.response.send_message('> Command is missing a parameter: `/wiki vehicle_name`', ephemeral=True)
     else:
         general_stats_list = ['Faction', 'Vehicle Type', 'Disabled Under', 'Repair Cost', 'Crew', 'Inventory Slots', 'Cost']
         armament_stats_list = ['Ammo', 'Maximum Range', 'Reload Time', 'Range']
-        faction_color = {'Warden': 0x245682, 'Colonial': 0x516C4B, 'Both': 0xffffff}
+
 
         vehicle_name = vehicle_name.lower()
-        vehicle_search_keys = fstats_utils.check_name_validity(vehicle_name)
+        vehicle_search_keys = wiki_utils.check_name_validity(vehicle_name)
         if not vehicle_search_keys:
-            await ctx.send('> Tank name is incorrect', ephemeral=True)
-        else:
-            embeds_list = []
-            vehicle_general_stats = fstats_utils.scrap_wiki_page(vehicle_search_keys)
-            vehicle_health_stats = fstats_utils.scrap_health_page(vehicle_search_keys)
+            await interaction.response.send_message('> Vehicle name is incorrect', ephemeral=True)
+            return
+        embeds_list = []
+        vehicle_general_stats = wiki_utils.scrap_wiki_page(vehicle_search_keys)
+        vehicle_health_stats = wiki_utils.scrap_health_page(vehicle_search_keys)
 
-            embed = discord.Embed(title=vehicle_search_keys[0], description=vehicle_general_stats['description'], color=faction_color[vehicle_general_stats['general']['Faction']])
-            embed.set_thumbnail(url=vehicle_general_stats['icon'])
+        embed = discord.Embed(
+            title=vehicle_search_keys[0],
+            description=vehicle_general_stats['description'],
+            color=factions_settings.FACTION_COLORS[vehicle_general_stats['general']['Faction']],
 
-            general_stats_list = [field for field in general_stats_list if field in vehicle_general_stats['general']]
+        )
+        embed.set_thumbnail(url=vehicle_general_stats['icon'])
 
-            embed.add_field(name='**HP**', value=vehicle_health_stats['HP'] if 'HP' in vehicle_health_stats else 'N/A', inline=False)
-            for stat in general_stats_list:
-                embed.add_field(name=stat, value=vehicle_general_stats['general'][stat], inline=True)
+        general_stats_list = [field for field in general_stats_list if field in vehicle_general_stats['general']]
 
-            if vehicle_general_stats['general']['Vehicle Type'] in fstats_utils.MILITARY_VEHICLE:
-                embed.add_field(name=u'\u200B', value='', inline=False)
-                embed.add_field(name='**Armament**', value='', inline=False)
-                for key, value in vehicle_general_stats['armament'].items():
-                    embed.add_field(name=key, value='', inline=False)
-                    armament_stats_list = [field for field in armament_stats_list if field in value] # allow to only embed tag present in the dict
-                    for stat in armament_stats_list:
-                        embed.add_field(name=stat, value=value[stat], inline=True)
-            embed.add_field(name='', value=f'[Wiki Page]({vehicle_search_keys[1]})', inline=False)
-            embeds_list.append(embed)
+        embed.add_field(
+            name='**HP**',
+            value=vehicle_health_stats['HP'] if 'HP' in vehicle_health_stats else 'N/A',
+            inline=False
+        )
+        for stat in general_stats_list:
+            embed.add_field(
+                name=stat,
+                value=vehicle_general_stats['general'][stat],
+                inline=True
+            )
 
-            vehicle_health_stats.pop('HP')
-            embed = discord.Embed(title='**Damage Type Resistance**', color=faction_color[vehicle_general_stats['general']['Faction']])
-            for key, value in vehicle_health_stats.items():
-                embed.add_field(name=key, value='', inline=False)
-                for subkey, sub_value in value.items():
-                    embed.add_field(name=subkey, value=sub_value, inline=True)
-                embed.add_field(name=u'\u200B', value='', inline=False)
-            embed.add_field(name='', value=f"[Wiki Page]({'https://foxhole.wiki.gg/wiki/Vehicle_Health'})", inline=False)
-            embeds_list.append(embed)
+        if vehicle_general_stats['general']['Vehicle Type'] in wiki_utils.MILITARY_VEHICLE:
+            embed.add_field(
+                name=u'\u200B',
+                value='',
+                inline=False
+            )
+            embed.add_field(
+                name='**Armament**',
+                value='',
+                inline=False
+            )
+            for key, value in vehicle_general_stats['armament'].items():
+                embed.add_field(
+                    name=key,
+                    value='',
+                    inline=False
+                )
+                armament_stats_list = [field for field in armament_stats_list if field in value] # allow to only embed tag present in the dict
+                for stat in armament_stats_list:
+                    embed.add_field(
+                        name=stat,
+                        value=value[stat],
+                        inline=True
+                    )
+        embed.add_field(
+            name='',
+            value=f'[Wiki Page]({vehicle_search_keys[1]})',
+            inline=False
+        )
+        embeds_list.append(embed)
 
-            await ctx.send(embeds=embeds_list, ephemeral=True)
+        vehicle_health_stats.pop('HP')
+        embed = discord.Embed(
+            title='**Damage Type Resistance**',
+            color=factions_settings.FACTION_COLORS[vehicle_general_stats['general']['Faction']]
+        )
+        for key, value in vehicle_health_stats.items():
+            embed.add_field(
+                name=key,
+                value='',
+                inline=False
+            )
+            for subkey, sub_value in value.items():
+                embed.add_field(
+                    name=subkey,
+                    value=sub_value,
+                    inline=True
+                )
+            embed.add_field(
+                name=u'\u200B',
+                value='',
+                inline=False
+            )
+        embed.add_field(
+            name='',
+            value=f"[Wiki Page]({'https://foxhole.wiki.gg/wiki/Vehicle_Health'})",
+            inline=False
+        )
+        embeds_list.append(embed)
+
+        await interaction.response.send_message(embeds=embeds_list, ephemeral=True)
 
 async def setup(bot):
-    bot.add_command(fstats)
+    bot.tree.add_command(wiki)
