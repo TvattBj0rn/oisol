ba4835cf6cabd1d2d482e3103850f84b03f7226d
Clément DELATTRE <clement.delattre@epitech.eu>
2023-08-28 15:19:04 +0200

    Remake of prototype version accessible from pc

--- modules/utils/path.py
+++ modules/utils/path.py
@@ -1,2 +1,7 @@
+import platform
+
 def generate_path(server_id: int, file: str) -> str:
-    return f'/oisol/{server_id}/{file}'
+    if platform.node() == 'fedora':
+        return f'./oisol/{server_id}/{file}'
+    else:
+        return f'/oisol/{server_id}/{file}'
