efbc12ccd4f28b2d120f5570de3cfd31e5a2a09e
Clément DELATTRE <clement.delattre@epitech.eu>
2023-11-02 17:16:58 +0100

    Add continue to prevent overlaping renames

--- modules/single_commdands/entrypoint_single_commands.py
+++ modules/single_commdands/entrypoint_single_commands.py
@@ -14,14 +14,20 @@ class ModuleSingleCommands(commands.Cog):
         for guild_member in guild_members:
             if guild_member.get_role(1125790881111359494) and guild_member.display_name[:8] != '[FCF] ⁂ ':
                 await guild_member.edit(nick=f'[FCF] ⁂ {guild_member.display_name}')
+                continue
             elif guild_member.get_role(1125790881111359493) and guild_member.display_name[:8] != '[FCF] ⁑ ':
                 await guild_member.edit(nick=f'[FCF] ⁑ {guild_member.display_name}')
+                continue
             elif guild_member.get_role(1125790881094570053) and guild_member.display_name[:8] != '[FCF] ∗ ':
                 await guild_member.edit(nick=f'[FCF] ∗ {guild_member.display_name}')
+                continue
             elif guild_member.get_role(1147568458532409474) and guild_member.display_name[:8] != '[FCF] ✪ ':
                 await guild_member.edit(nick=f'[FCF] ✪ {guild_member.display_name}')
+                continue
             elif guild_member.get_role(1125790881094570046) and guild_member.display_name[:8] != '[FCF] ⦿ ':
                 await guild_member.edit(nick=f'[FCF] ⦿ {guild_member.display_name}')
+                continue
             elif guild_member.get_role(1125790881094570045) and guild_member.display_name[:2] != '⦾ ':
                 await guild_member.edit(nick=f'⦾ {guild_member.display_name}')
+                continue
         await interaction.followup.send('> Les pseudos des membres ont bien été mis à jour', ephemeral=True)
