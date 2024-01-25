06e3048fd2a76c8189d9c685758722276eb47764
Clément DELATTRE <clement.delattre@epitech.eu>
2024-01-25 22:53:06 +0100

    Cross platform paths for Register & deprecated for the other modules

--- modules/utils/path.py
+++ modules/utils/path.py
@@ -1,19 +1,23 @@
 import platform
 from enum import Enum
+from deprecated import deprecated
 
 
+@deprecated(reason='This will be removed soon')
 def generate_path(server_id: int, file: str) -> str:
     if platform.node() == 'fedora':
         return f'./oisol/{server_id}/{file}'
     return f'/oisol/{server_id}/{file}'
 
 
+@deprecated(reason='This will be removed soon')
 def get_root_path() -> str:
     if platform.node() == 'fedora':
         return f'./oisol/'
     return f'/oisol/'
 
 
+@deprecated(reason='This will be removed soon')
 class DataFilesPath(Enum):
     REGISTER = 'register.csv'
     STOCKPILES = 'stockpiles.csv'
