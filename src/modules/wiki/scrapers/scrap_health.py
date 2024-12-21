0179eac039c57629d6ccb1d1a62b28cad3fbcc1f
Clément DELATTRE <clement.delattre@epitech.eu>
2024-12-21 21:16:47 +0100

    ruff RET rules fix

--- src/modules/wiki/scrapers/scrap_health.py
+++ src/modules/wiki/scrapers/scrap_health.py
@@ -22,6 +22,7 @@ def get_entry_row(tbody: Tag, headers_indexes: list, name: str) -> Tag | None:
         # It is assumed that Name will always be scrapped
         if tr.select('td')[headers_indexes.index('Name')].get_text(strip=True) in {name, name.removesuffix(' (Battleship)')}:
             return tr
+    return None
 
 
 def extract_td_data(td: Tag) -> dict | str:
