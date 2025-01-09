d4c219a2e4ec1e104957303d11ca830f6943ca71
Clément DELATTRE <clement.delattre@epitech.eu>
2025-01-09 12:38:45 +0100

    fix format

--- src/modules/wiki/scrapers/scrap_wiki.py
+++ src/modules/wiki/scrapers/scrap_wiki.py
@@ -64,7 +64,7 @@ def generate_infobox_data(infobox_soup: Tag) -> dict:
     data_dict = {
         'title': infobox_soup.find('h2', {'class': 'pi-item pi-item-spacing pi-title'}).get_text(),
         # For img_url, we want to use the highest resolution pic, if /thumb is found in img_path, we use its original picture instead
-        'img_url': '/'.join(f'https://foxhole.wiki.gg{img_path}'.replace('/thumb', '').split('/')[:-1]) if '/thumb' in (img_path := infobox_soup.select_one('a > img')['src']) else f"https://foxhole.wiki.gg{img_path}"}
+        'img_url': '/'.join(f'https://foxhole.wiki.gg{img_path}'.replace('/thumb', '').split('/')[:-1]) if '/thumb' in (img_path := infobox_soup.select_one('a > img')['src']) else f'https://foxhole.wiki.gg{img_path}'}
 
     merged_class = infobox_soup['class']
     if 'pi-theme-Col' in merged_class:
