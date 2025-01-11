1388f80118bf35d8fa95a843b2aa89cb1e441c07
Clément DELATTRE <clement.delattre@epitech.eu>
2025-01-11 19:38:11 +0100

    apply formula to single func

--- src/modules/wiki/mpf_generation.py
+++ src/modules/wiki/mpf_generation.py
@@ -3,14 +3,11 @@ import math
 
 def apply_mpf_formula(default_cost: int) -> [int]:
     """
-    Apply foxhole MPF formula, from a default resource cost
+    Apply foxhole MPF formula, from a default resource cost. See https://foxhole.wiki.gg/wiki/Mass_Production_Factory for formula.
     :param default_cost: default cost of an item
     :return: price reduction for each mpf tier
     """
-    costs_increase = [0.1, 0.2, 0.3, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5]
-    tier_cost_calculation = lambda tier_number: sum(default_cost - math.ceil(default_cost * costs_increase[i]) for i in range(tier_number))
-
-    return [tier_cost_calculation(i) for i in range(1, 10)]
+    return [sum(default_cost - math.ceil(default_cost * [0.1, 0.2, 0.3, 0.4, 0.5, 0.5, 0.5, 0.5, 0.5][j]) for j in range(i)) for i in range(1, 10)]
 
 
 def generate_mpf_data(production_costs: list[str]) -> dict:
