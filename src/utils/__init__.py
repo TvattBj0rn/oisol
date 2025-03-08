6d6a5e77d4ccb3696220adbc1dc04eb88f5a6514
Clément DELATTRE <67902539+TvattBj0rn@users.noreply.github.com>
2025-03-08 19:32:06 +0100

    fix logger not showing any python / discord info

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,4 +2,4 @@ from .functions import *
 from .oisol_enums import *
 from .resources import *
 from .foxhole_api_handler import FoxholeAPIWrapper
-from .oisol_logger import OisolLogger
+from .oisol_logger import OisolLogger, OisolFormatter
