2219efea6b00912636cce8ef98d530be9a2de0c0
Clément DELATTRE <67902539+TvattBj0rn@users.noreply.github.com>
2025-07-19 13:49:24 +0200

    remove persistent sqlite connector from data_cleaning_tasks.py

--- src/modules/data_cleaning_tasks.py
+++ src/modules/data_cleaning_tasks.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
+import sqlite3
 import time
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands, tasks
 
-from src.utils import InterfaceType
+from src.utils import InterfaceType, OISOL_HOME_PATH
 
 if TYPE_CHECKING:
     from main import Oisol
@@ -17,31 +18,35 @@ class DatabaseCleaner(commands.Cog):
         self.bot = bot
         self.remove_non_existing_interfaces.start()
 
-    def _clear_entries(self, channel_id: int, message_id: int, interface_type: str, interface_reference: str) -> None:
+    @staticmethod
+    def _clear_entries(conn_cursor: tuple[sqlite3.Connection, sqlite3.Cursor], channel_id: int, message_id: int, interface_type: str, interface_reference: str) -> None:
         for table, column in InterfaceType[interface_type].value:
-            self.bot.cursor.execute(f"DELETE FROM {table} WHERE {column} == '{interface_reference}'")
-        self.bot.cursor.execute(
+            conn_cursor[1].execute(f"DELETE FROM {table} WHERE {column} == '{interface_reference}'")
+        conn_cursor[1].execute(
             f'DELETE FROM AllInterfacesReferences WHERE ChannelId == {channel_id} AND MessageId == {message_id}',
         )
-        self.bot.connection.commit()
+
+        conn_cursor[0].commit()
 
     @tasks.loop(hours=24)
     async def remove_non_existing_interfaces(self) -> None:
         start_time = time.time()
-        all_existing_interfaces = self.bot.cursor.execute(
-            'SELECT ChannelId, MessageId, InterfaceType, InterfaceReference FROM AllInterfacesReferences',
-        ).fetchall()
-        for channel_id, message_id, interface_type, interface_reference in all_existing_interfaces:
-            channel = self.bot.get_channel(channel_id)
-            if channel is None:
-                self._clear_entries(channel_id, message_id, interface_type, interface_reference)
-                continue
-            try:
-                await channel.fetch_message(message_id)
-            except discord.NotFound:
-                # Associated message was deleted
-                self._clear_entries(channel_id, message_id, interface_type, interface_reference)
-            except (discord.Forbidden, discord.HTTPException):
-                # Rights of the bot have been removed or fail on network part
-                continue
+        with sqlite3.connect(OISOL_HOME_PATH / 'oisol.db') as conn:
+            cursor = conn.cursor()
+            all_existing_interfaces = cursor.execute(
+                'SELECT ChannelId, MessageId, InterfaceType, InterfaceReference FROM AllInterfacesReferences',
+            ).fetchall()
+            for channel_id, message_id, interface_type, interface_reference in all_existing_interfaces:
+                channel = self.bot.get_channel(channel_id)
+                if channel is None:
+                    self._clear_entries((conn, cursor), channel_id, message_id, interface_type, interface_reference)
+                    continue
+                try:
+                    await channel.fetch_message(message_id)
+                except discord.NotFound:
+                    # Associated message was deleted
+                    self._clear_entries((conn, cursor), channel_id, message_id, interface_type, interface_reference)
+                except (discord.Forbidden, discord.HTTPException):
+                    # Rights of the bot have been removed or fail on network part
+                    continue
         self.bot.logger.task(f'[TASK] remove_non_existing_interface task complete in {time.time() - start_time}s')
