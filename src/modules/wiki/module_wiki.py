907bddfe66db69d853601f56c1192ad2ebfb9fb3
Clément DELATTRE <67902539+TvattBj0rn@users.noreply.github.com>
2025-09-05 14:40:31 +0200

    fix resolved entry being always used even when non resolved is valid

--- src/modules/wiki/module_wiki.py
+++ src/modules/wiki/module_wiki.py
@@ -91,9 +91,12 @@ class ModuleWiki(commands.Cog):
                 # Maps targets are converted to their associated structures
                 health_table = WikiTables.STRUCTURES.value
 
-            data_dict = await wrapper.retrieve_row_data_from_table(table_fields[health_table], health_table, resolved_search_request)
+            # Search request can include tiered entries while resolved serve as a backup
+            final_search_request = resolved_search_request if health_table_redirect is None else search_request
 
-        data_dict['name'] = resolved_search_request
+            data_dict = await wrapper.retrieve_row_data_from_table(table_fields[health_table], health_table, final_search_request)
+
+        data_dict['name'] = final_search_request
         health_embed = HealthEntryEngine(data_dict).get_generated_embed()
 
         await interaction.response.send_message(embed=discord.Embed.from_dict(health_embed), ephemeral=not visible)
@@ -105,14 +108,14 @@ class ModuleWiki(commands.Cog):
             current = 'foxhole' # when user input is empty, search for a default value foxhole
 
         async with FoxholeWikiAPIWrapper() as wrapper:
-            # Get raw search results
-            search_results_redirect = await wrapper.wiki_search_request(current, do_resolve_redirect=False)
+            # Get non redirected search results
+            search_results = await wrapper.wiki_search_request(current, do_resolve_redirect=False)
 
             # Create a mask of valid entries from raw results (not a page but a redirect are ignored for example)
-            mask = await asyncio.gather(*(wrapper.is_page_wiki_page(wrapper.get_active_session(), table) for table in list(search_results_redirect)))
+            mask = await asyncio.gather(*(wrapper.is_page_wiki_page(wrapper.get_active_session(), table) for table in list(search_results)))
 
         # Get valid entries from the mask
-        search_results_redirect = compress(list(search_results_redirect), mask)
+        search_results_redirect = compress(list(search_results), mask)
 
         # Manual corrections to add all types of a specific entry,
         # Replace k by k: k + vv for vv in v
@@ -123,6 +126,12 @@ class ModuleWiki(commands.Cog):
             'Post Office': tier_iterable,
             'School': tier_iterable,
             'Bunker Base': tier_iterable,
+            'Wall': tier_iterable,
+            'Gate': tier_iterable,
+            'Bunker': tier_iterable,
+            'Bunker Ramp': tier_iterable,
+            'Bunker Corner': tier_iterable,
+            'Observation Bunker': tier_iterable,
             'Garrisoned House': [
                 '- Small (Tier 1)', '- Medium (Tier 1)', '- Large (Tier 1)',
                 '- Small (Tier 2)', '- Medium (Tier 2)', '- Large (Tier 2)',
