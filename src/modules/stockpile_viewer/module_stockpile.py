d7170d7574bad707a4b0134a573c7422e64c2c11
Clément DELATTRE <67902539+TvattBj0rn@users.noreply.github.com>
2025-10-18 22:15:06 +0200

    typo

--- src/modules/stockpile_viewer/module_stockpile.py
+++ src/modules/stockpile_viewer/module_stockpile.py
@@ -148,7 +148,7 @@ class ModuleStockpiles(commands.Cog):
             embed=discord.Embed.from_dict({
                 'title': f'<:region:1130915923704946758> | Stockpiles | {name}',
                 'color': Faction[guild_faction].value,
-                'description': '- **View Stockpiles** button: will display more or less stockpiles to the user depedning on its level of access to the interface (1-5)\n'
+                'description': '- **View Stockpiles** button: will display more or less stockpiles to the user depending on its level of access to the interface (1-5)\n'
                                '- **Share ID** button: available only to the creator of the interface, get the association ID of the interface to share with other server(s)',
             }),
             view=StockpilesViewMenu(),
