a04333326cf87a86a1f436dd48775e60038daa5d
Clément DELATTRE <67902539+TvattBj0rn@users.noreply.github.com>
2025-10-19 17:27:46 +0200

    add permission edit button

--- src/modules/stockpile_viewer/stockpile_view_menu.py
+++ src/modules/stockpile_viewer/stockpile_view_menu.py
@@ -12,7 +12,7 @@ from src.utils import (
     OISOL_HOME_PATH,
     OisolLogger,
     PriorityType,
-    DataFilesPath,
+    DataFilesPath, InterfacesTypes,
 )
 
 
@@ -24,9 +24,18 @@ class StockpilesViewMenu(discord.ui.View):
     async def display_stockpiles(self, interaction: discord.Interaction, _button: discord.ui.Button) -> None:
         pass
 
+    @discord.ui.button(style=discord.ButtonStyle.blurple, custom_id='Stockpile:Permissions', label='Configure Permissions', emoji='🔑')
+    async def edit_interface_permissions(self, interaction: discord.Interaction, _button: discord.ui.Button) -> None:
+        pass
+
     @discord.ui.button(style=discord.ButtonStyle.blurple, custom_id='Stockpile:Share', label='Share ID', emoji='🔗')
     async def get_stockpile_association_id(self, interaction: discord.Interaction, _button: discord.ui.Button) -> None:
         if interaction.user.name != interaction.message.interaction_metadata.user.name:
             await interaction.response.send_message('> Only the creator of the interface can do this action', ephemeral=True)
         else:
+            with sqlite3.connect(OISOL_HOME_PATH / 'oisol.db') as conn:
+                potential_association_id_as_list = conn.cursor().execute(
+                    'SELECT AssociationId FROM AllInterfacesReferences WHERE GroupId == ? AND ChannelId == ? AND MessageId == ? AND InterfaceType IN (?, ?)',
+                    (interaction.guild_id, interaction.channel_id, interaction.message.id, InterfacesTypes.STOCKPILE.value, InterfacesTypes.MULTISERVER_STOCKPILE.value),
+                ).fetchall()
             await interaction.response.send_message('> The association id is: ``', ephemeral=True)
