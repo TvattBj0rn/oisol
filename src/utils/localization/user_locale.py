3d718aa72c74e41d846c54993c48d6e8b0e23e89
Vask <67902539+TvattBj0rn@users.noreply.github.com>
2026-02-01 21:27:43 +0100

    add user locales strings

---
+++ src/utils/localization/user_locale.py
@@ -0,0 +1,98 @@
+USER_LOCALE_LOCALIZATION_DICT = {
+    'french': {
+        ## USER LOCALE
+        # Module Translation
+        'Translate': 'Traduire',
+        # Module Register
+        'Command to display the current list of recruit with the date the got the recruit role': "Affiche la liste des recrues et leur date d'obtention du rôle recrue",
+        # Module Stockpiles
+        'Create a new stockpile interface': 'Créer une nouvelle interface de stocks',
+        'Join an existing stockpile interface shared between multiple servers': "Rejoindre un réseau d'interfaces multi serveur",
+        'Update up to 5 stockpiles codes from a list': "Modifier jusqu'à 5 stockpiles",
+        'Clear a specific interface': 'Nettoyer une interface',
+        'Clear a given interface, and its network if connected to other interfaces': "Nettoyer une interface (et son réseau si connectée à d'autres interfaces)",
+        'Create a new stockpile on a given interface/network': 'Créer un nouveau stock sur un(e) interface/réseau',
+        'Create multiple stockpiles at once on a given interface/network': 'Créer plusieurs stocks sur un(e) interface/réseau',
+        'Delete an existing stockpile on a given interface/network': 'Supprimer un stock sur un(e) interface/réseau',
+        'Delete multiple existing stockpiles on a given interface/network': 'Supprimer plusieurs stocks sur un(e) interface/réseau',
+        # Module Todolist
+        'Create a new todolist interface, with option to limit the interface usage to specific role(s) / user(s)': 'Créer un nouvelle interface de tâches, avec possibilité de limiter les usages à des rôles/utilisateurs spécifiques',
+        # Module Config
+        'Command to add missing config, with possibility to reset the existing configuration': 'Compléter la configuraiton avec des éléments manquants, avec possibilité de remettre à zéro la config',
+        'Display the current server configuration': 'Afficher la configuration actuelle sur ce serveur',
+        'Set the recruit discord role, icons for recruits & promoted recruits as well as the option to update or not the symbol on promotion': "Définir le rôle recrue, les symboles recrues et le fait de mettre à jour le symbole d'une recrue promue",
+        'Set the language the bot uses for the server': 'Définir le langage que le bot utilise pour le serveur',
+        'Set the name of the group using the bot': 'Définir le nom de la guilde',
+        'Set the tag of the regiment group using the bot': 'Definir le tag de la guilde',
+        'Set the shard of the group (default is Able), this can impact the results of the stockpiles creation & health commands (shard dependant)': 'Définir le serveur Foxhole de la guilde (Able par défaut), cette valeur impacte les réponses des commandes de création de stocks et de health',
+        'Set the faction of the group using the bot, this will only impact the color of the stockpile interface': "Définir la faction de la guilde, (cela impactera la couleur de l'interface de stocks)",
+        # Module Wiki
+        'Get a wiki infobox': 'Afficher une infobox wiki',
+        'List each required ammunition to destroy a given vehicle / structure': 'Afficher la liste des munitions pouvant détruire un véhicule/structure et leurs nombre',
+        'Production cost of a given entry, with all possibilities (Factory, MPF, Facility)': "Coûts de production d'une entrée wiki (Usine, MPF, ...)",
+    },
+    'portuguese': {
+        ## USER LOCALE
+        # Module Translation
+        'Translate': 'Traduzir',
+        # Module Register
+        'Command to display the current list of recruit with the date the got the recruit role': 'Comando que exibe a lista atual do cargo recruta com a data em que receberam esse cargo',
+        # Module Stockpiles
+        'Create a new stockpile interface': 'Cria uma nova interface de stockpiles',
+        'Join an existing stockpile interface shared between multiple servers': 'Entra em uma interface de stockpiles existente compartilhada entre vários servidores',
+        'Update up to 5 stockpiles codes from a list': 'Atualiza até 5 códigos de stockpiles a partir de uma lista',
+        'Clear a specific interface': 'Limpa uma interface específica',
+        'Clear a given interface, and its network if connected to other interfaces': 'Limpa uma interface e sua rede de conexões, caso esteja conectada a outras interfaces',
+        'Create a new stockpile on a given interface/network': 'Cria um novo stockpile em uma interface/rede específica',
+        'Create multiple stockpiles at once on a given interface/network': 'Cria vários stockpiles de uma vez em uma interface/rede específica',
+        'Delete an existing stockpile on a given interface/network': 'Exclui um stockpile existente em uma interface/rede específica',
+        'Delete multiple existing stockpiles on a given interface/network': 'Exclui vários stockpiles existentes em uma interface/rede específica',
+        # Module Todolist
+        'Create a new todolist interface, with option to limit the interface usage to specific role(s) / user(s)': 'Cria uma nova interface de lista de tarefas, com opção de limitar o uso a cargos/usuários específicos',
+        # Module Config
+        'Command to add missing config, with possibility to reset the existing configuration': 'Cria uma nova interface de lista de tarefas, com opção de limitar o uso a cargos/usuários específicos',
+        'Display the current server configuration': 'Exibe a configuração atual do servidor',
+        'Set the recruit discord role, icons for recruits & promoted recruits as well as the option to update or not the symbol on promotion': 'Define o cargo de recruta no Discord, os ícones para recrutas e recrutas promovidos, e a opção de atualizar (ou não) o símbolo na promoção',
+        'Set the language the bot uses for the server': 'Define o idioma que o bot usa no servidor',
+        'Set the name of the group using the bot': 'Define o nome do regimento/grupo que está usando o bot',
+        'Set the tag of the regiment group using the bot': 'Define a tag do regimento/grupo que está usando o bot',
+        'Set the shard of the group (default is Able), this can impact the results of the stockpiles creation & health commands (shard dependant)': 'Define o shard do regimento/grupo (padrão: Able). Isso pode impactar os resultados dos comandos de criação de stockpiles e de status/saúde (dependem do shard)',
+        'Set the faction of the group using the bot, this will only impact the color of the stockpile interface': 'Define a facção do regimento/grupo que usa o bot. Isso afeta apenas a cor da interface de stockpiles',
+        # Module Wiki
+        'Get a wiki infobox': 'Obtém uma infobox da wiki',
+        'List each required ammunition to destroy a given vehicle / structure': 'Lista toda a munição necessária para destruir um veículo/estrutura específico',
+        'Production cost of a given entry, with all possibilities (Factory, MPF, Facility)': 'Exibe o custo de produção de um item específico, com todas as possibilidades (Fábrica, MPF, Facility)',
+    },
+    'spanish': {
+        ## USER LOCALE
+        # Module Translation
+        'Translate': 'Traducir',
+        # Module Register
+        'Command to display the current list of recruit with the date the got the recruit role': 'Comando para mostrar la lista actual de reclutas con la fecha en la que obtuvieron el rol de recluta',
+        # Module Stockpiles
+        'Create a new stockpile interface': 'Crear una nueva interfaz de existencias',
+        'Join an existing stockpile interface shared between multiple servers': 'Unirse a una interfaz de almacenamiento existente compartida entre varios servidores',
+        'Update up to 5 stockpiles codes from a list': 'Actualizar hasta 5 códigos de existencias de una lista',
+        'Clear a specific interface': 'Borrar una interfaz específica',
+        'Clear a given interface, and its network if connected to other interfaces': 'Borrar una interfaz determinada y su red si está conectada a otras interfaces',
+        'Create a new stockpile on a given interface/network': 'Crear una nueva reserva en una interfaz/red determinada',
+        'Create multiple stockpiles at once on a given interface/network': 'Crear múltiples reservas a la vez en una interfaz/red determinada',
+        'Delete an existing stockpile on a given interface/network': 'Eliminar una reserva existente en una interfaz/red determinada',
+        'Delete multiple existing stockpiles on a given interface/network': 'Eliminar múltiples reservas existentes en una interfaz/red determinada',
+        # Module Todolist
+        'Create a new todolist interface, with option to limit the interface usage to specific role(s) / user(s)': 'Cree una nueva interfaz de lista de tareas pendientes, con la opción de limitar el uso de la interfaz a roles o usuarios específicos.',
+        # Module Config
+        'Command to add missing config, with possibility to reset the existing configuration': 'Comando para agregar configuración faltante, con posibilidad de restablecer la configuración existente',
+        'Display the current server configuration': 'Mostrar la configuración actual del servidor',
+        'Set the recruit discord role, icons for recruits & promoted recruits as well as the option to update or not the symbol on promotion': 'Establese el rol de Discord del recluta, los íconos para los reclutas y los reclutas promovidos, así como la opción de actualizar o no el símbolo en la promoción.',
+        'Set the language the bot uses for the server': 'Establese el idioma que utiliza el bot para el servidor',
+        'Set the name of the group using the bot': 'Establese el nombre del grupo usando el bot',
+        'Set the tag of the regiment group using the bot': 'Establese la etiqueta del grupo del regimiento usando el bot',
+        'Set the shard of the group (default is Able), this can impact the results of the stockpiles creation & health commands (shard dependant)': 'Establese el servidor del grupo (el valor predeterminado es Able), esto puede afectar los resultados de la creación de reservas y los comandos de estado (depende del servidor)',
+        'Set the faction of the group using the bot, this will only impact the color of the stockpile interface': 'Establese la facción del grupo usando el bot, esto solo afectará el color de la interfaz de almacenamiento.',
+        # Module Wiki
+        'Get a wiki infobox': 'Obtener un cuadro de información wiki',
+        'List each required ammunition to destroy a given vehicle / structure': 'Enumera cada munición necesaria para destruir un vehículo o estructura determinados.',
+        'Production cost of a given entry, with all possibilities (Factory, MPF, Facility)': 'Coste de producción de una entrada determinada, con todas las posibilidades (Fábrica, MPF, Instalación)',
+    },
+}
